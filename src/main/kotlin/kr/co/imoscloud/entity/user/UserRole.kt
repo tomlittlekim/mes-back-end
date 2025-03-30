bf338b7f08d3fcbff221ea10b51661ffec1d155d
털보 <mjkim@8pin.co.kr>
2025-03-30 22:06:18 +0900

    feat: UserRole entity, repository

---
+++ src/main/kotlin/kr/co/imoscloud/entity/user/UserRole.kt
@@ -0,0 +1,33 @@
+package kr.co.imoscloud.entity.user
+
+import jakarta.persistence.*
+import kr.co.imoscloud.entity.CommonCol
+
+@Entity
+@Table(name = "USER_ROLE")
+class UserRole(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ROLE_ID")
+    val id: Long = 0L,
+
+    @Column(name = "SITE", length = 40, nullable = false)
+    val site: String,
+
+    @Column(name = "COMP_CD", length = 40, nullable = false)
+    val compCd: String,
+
+    @Column(name = "PRIORITY_LEVEL", length = 20)
+    val priorityLevel: String? = null,
+
+    @Column(name = "ROLE_NAME", length = 100, nullable = false)
+    val roleName: String,
+
+    @Column(name = "FLAG_DEFAULT")
+    val flagDefault: Boolean = false,
+
+    @Column(name = "SEQUENCE")
+    val sequence: Int? = null
+
+): CommonCol()

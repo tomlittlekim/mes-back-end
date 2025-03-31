680ed498601a5d4e3e12b1cd53f5e4d47e4df739
털보 <mjkim@8pin.co.kr>
2025-03-31 15:36:19 +0900

    feat: Company entity, repository

---
+++ src/main/kotlin/kr/co/imoscloud/dto/CompanyDto.kt
@@ -0,0 +1,6 @@
+package kr.co.imoscloud.dto
+
+data class CompanySummery(
+    val id: Long,
+    val companyName: String?,
+)
--- src/main/kotlin/kr/co/imoscloud/repository/company/CompanyRepository.kt
+++ src/main/kotlin/kr/co/imoscloud/repository/company/CompanyRepository.kt
@@ -2,6 +2,9 @@ package kr.co.imoscloud.repository.company
 
 import kr.co.imoscloud.entity.company.Company
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
 interface CompanyRepository: JpaRepository<Company, Long> {
+    fun findByCompCd(compCd: String): Optional<Company>
+    fun findAllByCompCdIn(compCdList: List<String>): List<Company>
 }

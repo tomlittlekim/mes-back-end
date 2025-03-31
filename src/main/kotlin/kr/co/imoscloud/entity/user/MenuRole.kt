1bdabf5eb93aeb8e368f5a78e180a031bd91142d
털보 <mjkim@8pin.co.kr>
2025-03-31 15:37:35 +0900

    feat: MenuRole entity

---
+++ src/main/kotlin/kr/co/imoscloud/entity/user/MenuRole.kt
@@ -0,0 +1,42 @@
+package kr.co.imoscloud.entity.user
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "MENU_ROLE")
+class MenuRole(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L,
+
+    @Column(name = "ROLE_ID", length = 40, nullable = false)
+    var roleId: String,
+
+    @Column(name = "MENU_ID", length = 40, nullable = false)
+    var menuId: String,
+
+    @Column(name = "IS_OPEN")
+    var isOpen: Boolean = false,
+
+    @Column(name = "IS_DELETE")
+    var isDelete: Boolean = false,
+
+    @Column(name = "IS_INSERT")
+    var isInsert: Boolean = false,
+
+    @Column(name = "IS_ADD")
+    var isAdd: Boolean = false,
+
+    @Column(name = "IS_POPUP")
+    var isPopup: Boolean = false,
+
+    @Column(name = "IS_PRINT")
+    var isPrint: Boolean = false,
+
+    @Column(name = "IS_SELECT")
+    var isSelect: Boolean = false,
+
+    @Column(name = "IS_UPDATE")
+    var isUpdate: Boolean = false
+)

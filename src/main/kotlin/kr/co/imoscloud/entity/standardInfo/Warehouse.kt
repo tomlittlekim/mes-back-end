fbb52e60e40c1ec99d71aef0f2728bcd29d68333
syh <antlfn123@naver.com>
2025-04-01 17:05:28 +0900

    [feature] 창고정보관리 1.CRUD

---
+++ src/main/kotlin/kr/co/imoscloud/entity/standardInfo/Warehouse.kt
@@ -0,0 +1,46 @@
+package kr.co.imoscloud.entity.standardInfo
+
+import jakarta.persistence.*
+import java.time.LocalDate
+
+@Entity
+@Table(name = "WAREHOUSE")
+class Warehouse (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "SEQ", nullable = false)
+    var id: Int? = null,
+
+    @Column(name = "SITE", nullable = false, length = 20)
+    var site: String? = null,
+
+    @Column(name = "COMP_CD", nullable = false, length = 20)
+    var compCd: String? = null,
+
+    @Column(name = "FACTORY_ID", nullable = false, length = 50)
+    var factoryId: String? = null,
+
+    @Column(name = "WAREHOUSE_ID", nullable = false, length = 50)
+    var warehouseId: String? = null,
+
+    @Column(name = "WAREHOUSE_NAME", nullable = false, length = 50)
+    var warehouseName: String? = null,
+
+    @Column(name = "WAREHOUSE_TYPE", length = 20)
+    var warehouseType: String? = null,
+
+    @Column(name = "FLAG_ACTIVE")
+    var flagActive: Boolean? = null,
+
+    @Column(name = "CREATE_USER", length = 100)
+    var createUser: String? = null,
+
+    @Column(name = "CREATE_DATE")
+    var createDate: LocalDate? = null,
+
+    @Column(name = "UPDATE_USER", length = 100)
+    var updateUser: String? = null,
+
+    @Column(name = "UPDATE_DATE")
+    var updateDate: LocalDate? = null
+)

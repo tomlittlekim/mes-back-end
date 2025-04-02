af58e6962b3698966bc1176b333004820fbe6253
털보 <mjkim@8pin.co.kr>
2025-04-02 13:49:33 +0900

    ref: token 값을 기준으로 접속한 유저 찾아오는 서비스 - DB 조회에서 메모리 조회로 변경

--- src/main/kotlin/kr/co/imoscloud/security/CustomUserDetailsService.kt
+++ src/main/kotlin/kr/co/imoscloud/security/CustomUserDetailsService.kt
@@ -3,7 +3,6 @@ package kr.co.imoscloud.security
 import kr.co.imoscloud.core.Core
 import kr.co.imoscloud.repository.user.UserRepository
 import org.springframework.security.core.userdetails.UserDetails
-import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -15,10 +14,8 @@ class CustomUserDetailsService(
 
     @Transactional(readOnly = true)
     fun loadUserBySiteAndUserId(site: String, userId: String): UserDetails {
-        val user = userRepo.findBySiteAndLoginIdAndFlagActiveIsTrue(site, userId)
-            ?: throw UsernameNotFoundException("User not found with username: $userId")
-
-        val roleSummery = core.getUserRoleFromInMemory(user)
-        return UserPrincipal.create(user, roleSummery)
+        val loginUserSummery = core.getUserFromInMemory(userId)
+        val roleSummery = core.getUserRoleFromInMemory(loginUserSummery.roleId)
+        return UserPrincipal.create(loginUserSummery, roleSummery)
     }
 } 

ab91cdc4e09a9b6cd05530f3e237537e65659db4
털보 <mjkim@8pin.co.kr>
2025-04-09 07:59:08 +0900

    ref: MenuRoleService 분리 및 서비스 추가

---
+++ src/main/kotlin/kr/co/imoscloud/service/sysrtem/MenuRoleService.kt
@@ -0,0 +1,34 @@
+package kr.co.imoscloud.service.sysrtem
+
+import kr.co.imoscloud.core.Core
+import kr.co.imoscloud.dto.MenuRequest
+import kr.co.imoscloud.dto.MenuRoleResponse
+import kr.co.imoscloud.entity.user.MenuRole
+import kr.co.imoscloud.repository.user.MenuRepository
+import kr.co.imoscloud.util.SecurityUtils
+import org.springframework.stereotype.Service
+
+@Service
+class MenuRoleService(
+    private val core: Core,
+    private val menuRepo: MenuRepository
+) {
+
+    fun getMenuRoleGroup(): List<MenuRole> {
+        val loginUser = SecurityUtils.getCurrentUserPrincipal()
+        return core.getAllMenuRoleByRoleId(loginUser.roleId)
+    }
+
+    fun getMenuRole(menuId: String): MenuRole {
+        val loginUser = SecurityUtils.getCurrentUserPrincipal()
+        return core.getMenuRole(loginUser.roleId, menuId)
+            ?: throw IllegalArgumentException("Menu에 대한 권한 정보가 존재하지 않습니다. ")
+    }
+
+    fun getInitialMenuRole(roleId: Long): List<MenuRoleResponse> =
+        menuRepo.findAll().map { menu -> MenuRoleResponse(roleId = roleId, menuId = menu.menuId) }
+
+    fun upsertMenuRole(req: MenuRequest): String {
+        return ""
+    }
+}

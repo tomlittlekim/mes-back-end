2ae607a6357448fc7975275c61e871ca390a190e
hj4645 <hjpark4645@gmail.com>
2025-04-09 15:26:29 +0900

    chore: BOM, BOM Detatil, BOM Hist 엔티티 수정

--- src/main/kotlin/kr/co/imoscloud/entity/material/Bom.kt
+++ src/main/kotlin/kr/co/imoscloud/entity/material/Bom.kt
@@ -7,33 +7,30 @@ import java.time.LocalDateTime
 
 @Entity
 @Table(name = "BOM",)
-class Bom : CommonCol() {
+class Bom(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "SEQ", nullable = false)
-    var seq: Int? = null
+    var seq: Int? = null,
 
     @Column(name = "SITE", nullable = false, length = 50)
-    var site: String? = null
+    var site: String? = null,
 
     @Column(name = "COMP_CD", nullable = false, length = 50)
-    var compCd: String? = null
+    var compCd: String? = null,
 
     @Column(name = "BOM_ID", nullable = false, length = 50)
-    var bomId: String? = null
+    var bomId: String? = null,
 
     @Column(name = "BOM_LEVEL", nullable = false)
-    var bomLevel: Int = 1
+    var bomLevel: Int = 1,
 
     @Column(name = "ITEM_CD", nullable = false, length = 50)
-    var itemCd: String? = null
+    var itemCd: String? = null,
 
-    @Column(name = "BOM_NM", length = 100)
-    var bomNm: String? = null
+    @Column(name = "BOM_NAME", length = 100)
+    var bomName: String? = null,
 
     @Column(name = "REMARK", length = 500)
-    var remark: String? = null
-
-    @OneToMany(mappedBy = "bom", cascade = [CascadeType.ALL])
-    var bomDetails: MutableList<BomDetail> = mutableListOf()
-} 
+    var remark: String? = null,
+): CommonCol()

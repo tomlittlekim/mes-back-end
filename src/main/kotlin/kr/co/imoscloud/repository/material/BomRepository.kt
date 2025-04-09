71712fd68a0b8633dd7fea0b973082c0d7a1e31e
hj4645 <hjpark4645@gmail.com>
2025-04-09 15:30:26 +0900

    feat: BOM 좌측 그리드 CRUD + 우측 그리드 Read까지

---
+++ src/main/kotlin/kr/co/imoscloud/repository/material/BomRepository.kt
@@ -0,0 +1,101 @@
+package kr.co.imoscloud.repository.material
+
+import jakarta.transaction.Transactional
+import kr.co.imoscloud.entity.material.Bom
+import kr.co.imoscloud.model.material.BomDetailMaterialDto
+import kr.co.imoscloud.model.material.BomMaterialDto
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import java.time.LocalDateTime
+
+interface BomRepository : JpaRepository<Bom, Int> {
+    @Query(
+        """
+        SELECT new kr.co.imoscloud.model.material.BomMaterialDto(
+            b, 
+            m.materialType,
+            m.materialCategory,
+            m.userMaterialId,
+            m.materialName,
+            m.materialStandard,
+            m.unit
+        )
+        FROM Bom b
+        LEFT JOIN MaterialMaster m 
+            ON m.site = b.site 
+            AND m.compCd = b.compCd 
+            AND m.systemMaterialId = b.itemCd
+        WHERE b.site = :site
+        AND b.compCd = :compCd
+        AND (:materialType = '' OR m.materialType = :materialType)
+        AND (:materialName = '' OR m.materialName LIKE CONCAT('%', :materialName, '%'))
+        AND (:bomName = '' OR b.bomName LIKE CONCAT('%', :bomName, '%'))
+        AND b.flagActive = true
+        ORDER BY b.createDate DESC
+        """
+    ) // BOM 정보 호출
+    fun getBomList(
+        site: String,
+        compCd: String,
+        materialType: String?,
+        materialName: String?,
+        bomName: String?,
+//        flagActive: Boolean?
+    ): List<BomMaterialDto>
+
+    @Query(
+        """
+        SELECT new kr.co.imoscloud.model.material.BomMaterialDto(
+            b,
+            m.materialType,
+            m.materialCategory,
+            m.userMaterialId,
+            m.materialName,
+            m.materialStandard,
+            m.unit
+        )
+        FROM Bom b
+        LEFT JOIN MaterialMaster m 
+            ON m.site = b.site 
+            AND m.compCd = b.compCd 
+            AND m.systemMaterialId = b.itemCd
+        WHERE b.site = :site
+        AND b.compCd = :compCd
+        AND b.bomId IN :bomIds
+        AND b.flagActive = true
+        """
+    )
+    fun getBomByBomId(
+        site: String,
+        compCd: String,
+        bomIds: List<String>
+    ): List<BomMaterialDto>
+
+    @Query("""
+        SELECT b FROM Bom b
+        WHERE b.site = :site
+        AND b.compCd = :compCd
+        AND b.bomId = :bomId
+        AND b.flagActive = true
+    """)
+    fun findBomByBomId(
+        site: String,
+        compCd: String,
+        bomId: String
+    ): Bom?
+
+    @Modifying(clearAutomatically = true)
+    @Query("""
+        UPDATE Bom b 
+        SET b.flagActive = false
+        WHERE b.site = :site 
+        AND b.compCd = :compCd 
+        AND b.bomId = :bomId
+    """)
+    fun updateBomFlagActive(
+        site: String,
+        compCd: String,
+        bomId: String
+    ): Int
+} 

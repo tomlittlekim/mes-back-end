0e2488667a7f790d9b3e8a963b5ec7fbcb44aff3
털보 <mjkim@8pin.co.kr>
2025-04-13 22:08:57 +0900

    ref: 공지사항 생성 및 수정 서비스

--- src/main/kotlin/kr/co/imoscloud/service/sysrtem/NoticeService.kt
+++ src/main/kotlin/kr/co/imoscloud/service/sysrtem/NoticeService.kt
@@ -1,19 +1,21 @@
 package kr.co.imoscloud.service.sysrtem
 
 import jakarta.transaction.Transactional
+import kr.co.imoscloud.core.Core
 import kr.co.imoscloud.dto.NoticeSearchRequest
 import kr.co.imoscloud.dto.UpsertNoticeRequest
 import kr.co.imoscloud.entity.system.Notice
 import kr.co.imoscloud.repository.system.NoticeRepository
+import kr.co.imoscloud.util.AuthLevel
 import kr.co.imoscloud.util.DateUtils
 import kr.co.imoscloud.util.SecurityUtils
-import org.springframework.data.crossstore.ChangeSetPersister
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 import java.time.LocalTime
 
 @Service
 class NoticeService(
+    private val core: Core,
     private val noticeRepo: NoticeRepository,
 ) {
 
@@ -38,11 +40,46 @@ class NoticeService(
         return from
             ?.let { noticeRepo.findAllByCreateDateBetweenAndFlagActiveIsTrue(from, to!!) }
             ?: run { noticeRepo.findAll() }
-            .filter { validatePriorityLevel(loginUser.priorityLevel, it) }
+            .filter {
+                validatePriorityLevel(loginUser.priorityLevel, it)
+                && if (core.isDeveloper(loginUser)) true else it.flagActive
+            }
     }
 
-    fun upsertNotice(req: UpsertNoticeRequest): String { return "" }
+    @AuthLevel(minLevel = 5)
+    fun upsertNotice(req: UpsertNoticeRequest): String {
+        val loginUser = SecurityUtils.getCurrentUserPrincipal()
 
+        var upsertStr: String? = null
+        val notice: Notice = req.noticeId
+            ?.let { id ->
+                upsertStr = "수정"
+                noticeRepo.findByNoticeIdAndFlagActiveIsTrue(id)
+                    ?.let { n -> n.apply {
+                        noticeTitle = req.noticeTitle ?: this.noticeTitle
+                        noticeContents = req.noticeContents ?: this.noticeContents
+                        noticeWriter = loginUser.loginId
+                        attachmentPath = req.attachmentPath ?: this.attachmentPath
+                        noticeTtl = DateUtils.parseDateTime(req.noticeTtl) ?: this.noticeTtl
+                    } }
+                    ?: throw IllegalArgumentException("Not found notice with id: $id")
+            }
+            ?:run {
+                upsertStr = "생성"
+                Notice(
+                    noticeTitle = req.noticeTitle,
+                    noticeContents = req.noticeContents,
+                    noticeWriter = loginUser.loginId,
+                    attachmentPath = req.attachmentPath,
+                    noticeTtl = DateUtils.parseDateTime(req.noticeTtl),
+                ).apply { createCommonCol(loginUser) }
+            }
+
+        noticeRepo.save(notice)
+        return "${notice.noticeTitle} $upsertStr 성공"
+    }
+
+    @AuthLevel(minLevel = 5)
     fun deleteNotice(id: Long): String {
         val loginUser = SecurityUtils.getCurrentUserPrincipal()
 

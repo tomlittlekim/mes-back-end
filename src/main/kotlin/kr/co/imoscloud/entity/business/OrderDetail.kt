167047def5d28377400ab0a2e81d32dfb6aecdee
털보 <mjkim@8pin.co.kr>
2025-04-15 15:40:39 +0900

    feat: Order Table

--- src/main/kotlin/kr/co/imoscloud/entity/business/OrderDetail.kt
+++ src/main/kotlin/kr/co/imoscloud/entity/business/OrderDetail.kt
@@ -1,12 +1,46 @@
 package kr.co.imoscloud.entity.business
 
 import jakarta.persistence.*
+import kr.co.imoscloud.entity.CommonCol
+import java.time.LocalDate
 
 @Entity
 @Table(name = "ORDER_DETAIL")
-class OrderDetail {
+data class OrderDetail(
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null
-}
+    @Column(name = "ID")
+    val id: Long? = null,
+
+    @Column(name = "ORDER_NO", nullable = false, length = 100)
+    var orderNo: String,
+
+    @Column(name = "ORDER_SUB_NO", length = 100)
+    var orderSubNo: String? = null,
+
+    @Column(name = "SYSTEM_MATERIAL_ID", length = 50, nullable = false)
+    var systemMaterialId: String? = null,
+
+    @Column(name = "DELIVERY_DATE")
+    var deliveryDate: LocalDate? = null,
+
+    @Column(name = "QUANTITY", nullable = false)
+    var quantity: Int,
+
+    @Column(name = "UNIT_PRICE", nullable = false)
+    var unitPrice: Int,
+
+    @Column(name = "SUPPLY_PRICE")
+    var supplyPrice: Int? = null,
+
+    @Column(name = "VAT_PRICE")
+    var vatPrice: Int? = null,
+
+    @Column(name = "TOTAL_PRICE")
+    var totalPrice: Int? = null,
+
+    @Column(name = "REMARK", length = 100)
+    var remark: String? = null
+
+) : CommonCol()
--- src/main/kotlin/kr/co/imoscloud/entity/business/OrderHeader.kt
+++ src/main/kotlin/kr/co/imoscloud/entity/business/OrderHeader.kt
@@ -1,12 +1,49 @@
 package kr.co.imoscloud.entity.business
 
 import jakarta.persistence.*
+import kr.co.imoscloud.entity.CommonCol
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 @Entity
 @Table(name = "ORDER_HEADER")
-class OrderHeader {
+class OrderHeader(
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null
-}
+    @Column(name = "ID")
+    val id: Long? = null, // AutoIncrement PK
+
+    @Column(name = "ORDER_NO", unique = true, nullable = false, length = 100)
+    var orderNo: String,
+
+    @Column(name = "ORDER_DATE", nullable = false)
+    var orderDate: LocalDate? = null,
+
+    @Column(name = "CUSTOMER_ID", length = 100, nullable = false)
+    var customerId: String? = null,
+
+    @Column(name = "TOTAL_AMOUNT")
+    var totalAmount: Int? = null,
+
+    @Column(name = "VAT_AMOUNT", length = 100)
+    var vatAmount: String? = null,
+
+    @Column(name = "FLAG_VAT_AMOUNT")
+    var flagVatAmount: Boolean = false,  // 부가세 여부
+
+    @Column(name = "FINAL_AMOUNT")
+    var finalAmount: Int? = null,
+
+    @Column(name = "DELIVERY_DATE")
+    var deliveryDate: LocalDateTime? = null,
+
+    @Column(name = "PAYMENT_METHOD", length = 100)
+    var paymentMethod: String? = null,
+
+    @Column(name = "DELIVERY_ADDR", length = 100)
+    var deliveryAddr: String? = null,
+
+    @Column(name = "REMARK", length = 100)
+    var remark: String? = null
+): CommonCol()

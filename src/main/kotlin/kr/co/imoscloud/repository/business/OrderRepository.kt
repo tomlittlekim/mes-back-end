92d727bc8eaf531c8be878857f1c0f269ae59a5f
털보 <mjkim@8pin.co.kr>
2025-04-16 08:06:21 +0900

    feat: 영업:주문 헤더 조회 서비스

--- src/main/kotlin/kr/co/imoscloud/repository/business/OrderRepository.kt
+++ src/main/kotlin/kr/co/imoscloud/repository/business/OrderRepository.kt
@@ -3,9 +3,44 @@ package kr.co.imoscloud.repository.business
 import kr.co.imoscloud.entity.business.OrderDetail
 import kr.co.imoscloud.entity.business.OrderHeader
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.time.LocalDateTime
 
 interface OrderHeaderRepository: JpaRepository<OrderHeader, Long> {
+    @Query("""
+        select oh
+        from OrderHeader oh
+        where oh.compCd = :compcd
+            and (:orderNo is null or oh.orderNo = :orderNo)
+            and (:fromDate is null or (oh.createDate between :fromDate and :toDate))
+            and (:customerId is null or oh.customerId = :customerId)
+            and oh.flagActive is true 
+    """)
+    fun findAllBySearchCondition(
+        orderNo: String?=null,
+        fromDate: LocalDateTime?,
+        toDate: LocalDateTime?,
+        customerId: String?=null,
+    ): List<OrderHeader>
 }
 
 interface OrderDetailRepository: JpaRepository<OrderDetail, Long> {
+    @Query("""
+        select oh
+        from OrderDetail od
+        left join OrderHeader oh on od.orderNo = oh.orderNo
+        where od.compCd = :compcd
+            and (:orderNo is null or od.orderNo = :orderNo)
+            and (:fromDate is null or (od.createDate between :fromDate and :toDate))
+            and (:customerId is null or oh.customerId = :customerId)
+            and (:materialId is null or od.systemMaterialId = :materialId)
+            and od.flagActive is true 
+    """)
+    fun findAllBySearchCondition(
+        orderNo: String?=null,
+        fromDate: LocalDateTime?,
+        toDate: LocalDateTime?,
+        customerId: String?=null,
+        materialId: String?=null,
+    ): List<OrderHeader>
 }
--- src/main/kotlin/kr/co/imoscloud/service/business/OrderService.kt
+++ src/main/kotlin/kr/co/imoscloud/service/business/OrderService.kt
@@ -1,7 +1,10 @@
 package kr.co.imoscloud.service.business
 
+import kr.co.imoscloud.entity.business.OrderDetail
+import kr.co.imoscloud.entity.business.OrderHeader
 import kr.co.imoscloud.repository.business.OrderDetailRepository
 import kr.co.imoscloud.repository.business.OrderHeaderRepository
+import kr.co.imoscloud.util.DateUtils
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,5 +13,25 @@ class OrderService(
     val detailRepo: OrderDetailRepository
 ) {
 
+    // orderHeader 조회
+    fun getAllHeaderBySearchRequestByCompCd(req: OrderHeaderSearchRequest): List<OrderHeader> {
+        val (from, to) = DateUtils.getSearchDateRange(req.fromDate, req.toDate)
 
+        return req.materialId
+            ?.let { detailRepo.findAllBySearchCondition(req.orderNo, from, to, req.customerId, req.materialId) }
+            ?:run { headerRepo.findAllBySearchCondition(req.orderNo, from, to, req.customerId) }
+    }
+
+    data class OrderHeaderSearchRequest(
+        val orderNo: String?=null,
+        val fromDate: String?=null,
+        val toDate: String?=null,
+        val customerId: String?=null,
+        val materialId: String?=null,
+    )
+
+    data class OrderResponse(
+        val header: List<OrderHeader> = listOf(),
+        val details: List<OrderDetail>? = emptyList()
+    )
 }

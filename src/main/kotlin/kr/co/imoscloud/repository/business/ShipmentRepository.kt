2ec27067bee18beee932268a07086864b10c8032
털보 <mjkim@8pin.co.kr>
2025-04-21 13:46:45 +0900

    feat: 영업관리 출하 - 주문정보 조회 서비스

--- src/main/kotlin/kr/co/imoscloud/repository/business/ShipmentRepository.kt
+++ src/main/kotlin/kr/co/imoscloud/repository/business/ShipmentRepository.kt
@@ -2,9 +2,46 @@ package kr.co.imoscloud.repository.business
 
 import kr.co.imoscloud.entity.business.ShipmentDetail
 import kr.co.imoscloud.entity.business.ShipmentHeader
+import kr.co.imoscloud.service.business.ShipmentDetailNullableDto
+import kr.co.imoscloud.service.business.ShipmentHeaderNullableDto
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.time.LocalDateTime
 
 interface ShipmentHeaderRepository: JpaRepository<ShipmentHeader, Long> {
+    @Query("""
+        select new kr.co.imoscloud.service.business.ShipmentHeaderNullableDto(
+            sh.shipmentId as shipmentId,
+            sh.site as site,
+            sh.compCd as compCd,
+            sh.orderNo as orderNo,
+            oh.orderDate as orderDate,
+            oh.ordererId as ordererId,
+            oh.orderQuantity as orderQuantity,
+            oh.customerId as customerId,
+            oh.totalAmount as totalAmount,
+            sh.shipmentStatus as shipmentStatus,
+            sh.shippedQuantity as shippedQuantity,
+            sh.unshippedQuantity as unshippedQuantity,
+            sh.remark as remark
+        )
+        from ShipmentHeader sh
+        left join OrderHeader oh on sh.orderNo = oh.orderNo
+        where sh.compCd = :compCd
+            and (:orderNo is null or sh.orderNo = :orderNo)
+            and (:customerId is null or sh.customerId = :customerId)
+            and (:shipmentStatus is null or sh.shipmentStatus = :shipmentStatus)
+            and sh.createDate between :from and :to
+            and sh.flagActive is true 
+    """)
+    fun getAllBySearchCondition(
+        compCd: String?=null,
+        orderNo: String?=null,
+        customerId: String?=null,
+        shipmentStatus: String?=null,
+        from: LocalDateTime,
+        to: LocalDateTime,
+    ): List<ShipmentHeaderNullableDto>
 }
 
 interface ShipmentDetailRepository: JpaRepository<ShipmentDetail, Long> {
--- src/main/kotlin/kr/co/imoscloud/service/business/ShipmentService.kt
+++ src/main/kotlin/kr/co/imoscloud/service/business/ShipmentService.kt
@@ -2,14 +2,79 @@ package kr.co.imoscloud.service.business
 
 import kr.co.imoscloud.repository.business.ShipmentDetailRepository
 import kr.co.imoscloud.repository.business.ShipmentHeaderRepository
+import kr.co.imoscloud.util.DateUtils
+import kr.co.imoscloud.util.SecurityUtils
+import org.apache.catalina.security.SecurityUtil
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 
 @Service
 class ShipmentService(
-    private val orderService: OrderService,
     private val headerRepo: ShipmentHeaderRepository,
     private val detailRepo: ShipmentDetailRepository
 ) {
 
+    fun getHeadersBySearchRequest(req: ShipmentSearchRequest): List<ShipmentHeaderNullableDto> {
+        val loginUser = SecurityUtils.getCurrentUserPrincipal()
+        val (from, to) = DateUtils.getSearchDateRange(req.fromDate, req.toDate)
 
+        return headerRepo.getAllBySearchCondition(
+            loginUser.compCd,
+            req.orderNo,
+            req.customerId,
+            req.shipmentStatus,
+            from, to
+        )
+    }
 }
+
+data class ShipmentHeaderNullableDto(
+    val id: Long? = null,
+    val site: String? = null,
+    val compCd: String? = null,
+    val orderNo: String? = null,
+    //OrderHeader
+    val orderDate: LocalDate? = null,
+    val orderer: String? = null,
+    val orderQuantity: Int? = null,
+    val customerId: String? = null,
+    val totalAmount: Int? = 0,
+    //ShipmentHeader
+    val shipmentStatus: String?=null,
+    val shippedQuantity: Int? = null,
+    val unshippedQuantity: Int? = null,
+    val remark: String? = null,
+)
+
+data class ShipmentDetailNullableDto(
+    val id: Long? = null,
+    val site: String? = null,
+    val compCd: String? = null,
+    val orderNo: String? = null,
+    var orderSubNo: String? = null,
+    //OrderDetail
+    val systemMaterialId: String? = null,
+    val materialName: String? = null,
+    val materialStandard: String? = null,
+    val unit: String? = null,
+    var quantity: Int? = null,
+    //Warehouse
+    val stockQuantity: Int? = null,
+    //ShipmentHeader
+    var shipmentId: Long? = null,
+    var shipmentDate: LocalDate? = null,
+    var shippedQuantity: Int? = null,
+    var unshippedQuantity: Int? = null,
+    var cumulativeShipmentQuantity: Int? = null,
+    var shipmentWarehouse: String? = "제품창고",
+    var shipmentHandler: String? = null,
+    var remark: String? = null
+)
+
+data class ShipmentSearchRequest(
+    val orderNo: String?=null,
+    val fromDate: String?=null,
+    val toDate: String?=null,
+    val customerId: String?=null,
+    val shipmentStatus: String?=null,
+)

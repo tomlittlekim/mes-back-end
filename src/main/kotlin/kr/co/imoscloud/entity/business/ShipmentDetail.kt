c0d32b6aa2106087988ab663fdcbeb7d0862ee4e
털보 <mjkim@8pin.co.kr>
2025-04-21 13:46:28 +0900

    feat: 영업관리 출하 객체

--- src/main/kotlin/kr/co/imoscloud/entity/business/ShipmentDetail.kt
+++ src/main/kotlin/kr/co/imoscloud/entity/business/ShipmentDetail.kt
@@ -1,12 +1,59 @@
 package kr.co.imoscloud.entity.business
 
 import jakarta.persistence.*
+import kr.co.imoscloud.entity.CommonCol
+import kr.co.imoscloud.iface.DtoCompCdBase
+import java.time.LocalDate
 
 @Entity
 @Table(name = "SHIPMENT_DETAIL")
-class ShipmentDetail {
+class ShipmentDetail(
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null
-}
+    @Column(name = "ID")
+    var id: Long? = null,
+
+    @Column(name = "SITE", length = 20)
+    val site: String,
+
+    @Column(name = "COMP_CD", length = 20)
+    override val compCd: String,
+
+    @Column(name = "ORDER_NO", nullable = false, length = 100)
+    var orderNo: String,
+
+    @Column(name = "ORDER_SUB_NO", nullable = false, length = 100)
+    var orderSubNo: String,
+
+    @Column(name = "PRODUCT_WAREHOUSE_ID", length = 100)
+    var productWarehouseId: String? = null,
+
+    @Column(name = "SHIPMENT_ID", nullable = false)
+    var shipmentId: Long,
+
+    @Column(name = "SHIPMENT_DATE", nullable = false)
+    var shipmentDate: LocalDate? = null,
+
+    @Column(name = "SHIPPED_QUANTITY")
+    var shippedQuantity: Int? = null,
+
+    @Column(name = "UNSHIPPED_QUANTITY")
+    var unshippedQuantity: Int? = null,
+
+    @Column(name = "STOCK_QUANTITY")
+    var stockQuantity: Int? = null,
+
+    @Column(name = "CUMULATIVE_SHIPMENT_QUANTITY")
+    var cumulativeShipmentQuantity: Int? = null,
+
+    @Column(name = "SHIPMENT_WAREHOUSE")
+    var shipmentWarehouse: String? = null,
+
+    @Column(name = "SHIPMENT_HANDLER")
+    var shipmentHandler: String? = null,
+
+    @Column(name = "REMARK")
+    var remark: String? = null
+
+) : CommonCol(), DtoCompCdBase
--- src/main/kotlin/kr/co/imoscloud/entity/business/ShipmentHeader.kt
+++ src/main/kotlin/kr/co/imoscloud/entity/business/ShipmentHeader.kt
@@ -1,12 +1,40 @@
 package kr.co.imoscloud.entity.business
 
 import jakarta.persistence.*
+import kr.co.imoscloud.entity.CommonCol
+import kr.co.imoscloud.iface.DtoCompCdBase
 
 @Entity
 @Table(name = "SHIPMENT_HEADER")
-class ShipmentHeader {
+class ShipmentHeader(
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null
-}
+    @Column(name = "SHIPMENT_ID")
+    var shipmentId: Long? = null,
+
+    @Column(name = "SITE", length = 20)
+    val site: String,
+
+    @Column(name = "COMP_CD", length = 20)
+    override val compCd: String,
+
+    @Column(name = "ORDER_NO", unique = true, nullable = false)
+    var orderNo: String,
+
+    @Column(name = "CUSTOMER_ID", length = 100, nullable = false)
+    var customerId: String? = null,
+
+    @Column(name = "SHIPMENT_STATUS")
+    var shipmentStatus: String? = null,
+
+    @Column(name = "SHIPPED_QUANTITY")
+    var shippedQuantity: Int? = null,
+
+    @Column(name = "UNSHIPPED_QUANTITY")
+    var unshippedQuantity: Int? = null,
+
+    @Column(name = "REMARK")
+    var remark: String? = null,
+
+) : CommonCol(), DtoCompCdBase

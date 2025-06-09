6693e808ed6dbaaa4d3f19b79b344269df213ddf
syh <antlfn123@naver.com>
2025-06-09 13:40:26 +0900

    refactor: Enum 코드 변경

--- src/main/kotlin/kr/co/imoscloud/exception/ErrorCode.kt
+++ src/main/kotlin/kr/co/imoscloud/exception/ErrorCode.kt
@@ -7,10 +7,10 @@ enum class ErrorCode(
 ) {
 
     //유저 관련 정보
-    COMPANY_NOT_FOUND(403, "A001", "회사명을 찾을 수 없습니다."),
+    COMPANY_NOT_FOUND(404, "A001", "회사명을 찾을 수 없습니다."),
 
     //기기 관련 정보
-    DEVICE_NOT_FOUND(403,"E001","기기를 찾을 수 없습니다."),
+    DEVICE_NOT_FOUND(404,"E001","기기를 찾을 수 없습니다."),
 
     //업체 관련 정보
     NOT_EXIST_VENDOR_ID(400,"V001", "거래처ID가 존재하지 않습니다."),
